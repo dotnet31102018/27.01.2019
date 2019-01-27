6a07447ff6bc7fcaa7deb12adc9c895a31cbd26d
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-01-27 18:05:34 +0200

    Add files via upload

---
+++ HW2701/HW2701/Computer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2701
+{
+    [DebuggerDisplay("COMPUTER MODEL = {_price * 0.267}")]
+    class Computer
+    {
+        public string _model;
+        public int _price;
+        public int _numberOfProcessors;
+        public float _screenSize;
+        public bool _isTurnOn = false;
+
+        public int TellMeThePrice()
+        {
+            return _price;
+        }
+
+        public float TellMeTheScreenSize()
+        {
+            return _screenSize;
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Turning on the computer");
+            _isTurnOn = true;
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Turning off the computer");
+            _isTurnOn = false;
+        }
+
+        public void AddProcessor()
+        {
+            Console.WriteLine("Buying another processor");
+            _numberOfProcessors++;
+        }
+
+        public override string ToString()
+        {
+            return $"Computer model={_model} price={_price} number of processors = {_numberOfProcessors} screen size = {_screenSize} power = {(_isTurnOn ? "ON" : "OFF")}";
+        }
+    }
+}
---
+++ HW2701/HW2701/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer pc = new Computer
+            {
+                _isTurnOn = true,
+                _model = "DELL",
+                _numberOfProcessors = 16,
+                _price = 8599,
+                _screenSize = 17.1f
+            };
+
+            Computer mac = new Computer
+            {
+                _isTurnOn = true,
+                _model = "MAC BOOK PRO",
+                _numberOfProcessors = 8,
+                _price = 12778,
+                _screenSize = 15.3f
+            };
+
+            mac.TurnOff();
+
+            Console.WriteLine(mac);
+        }
+    }
+}
