b4a82095a05253eb0b1736179234ff6d6b425406
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-01-27 20:45:57 +0200

    Add files via upload

--- Shapes/_2701/Program.cs
+++ Shapes/_2701/Program.cs
@@ -42,6 +42,24 @@ namespace _2701
             TriangleRightAngle t3 = new TriangleRightAngle();
             PrintTriangle(t3);
 
+            int[] arr = { 1, 3, 4 };
+            int[] arr2 = new int[3];
+            arr2[0] = 1;
+            arr2[1] = 5;
+            arr2[2] = 3;
+
+            Circle[] circlesArray = { new Circle { _r = 2.2 } , new Circle { _r = 5.7 } };
+            Circle[] circles = new Circle[3];
+            circles[0] = new Circle
+            {
+                _r = 7.4
+            };
+
+            ShapeBase[] shapesArray = { new Circle(), new Triangle() };
+
+            ShapeBase[][] shapesJag = new ShapeBase[5][];
+
+
         }
 
         static void PrintCircleArea(Circle c2)
